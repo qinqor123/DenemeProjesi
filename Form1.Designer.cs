00248c33eb2c5912e071e0da24e7bea547d92e16
qinqor123 <101428389+qinqor123@users.noreply.github.com>
2022-03-27 23:57:22 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace DenemeProjesi
+{
+    partial class AnyOtp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(AnyOtp));
+            this.BtnKaydet = new System.Windows.Forms.Button();
+            this.BtnSil = new System.Windows.Forms.Button();
+            this.BtnYukle = new System.Windows.Forms.Button();
+            this.BtnIptal = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // BtnKaydet
+            // 
+            this.BtnKaydet.Location = new System.Drawing.Point(3, 224);
+            this.BtnKaydet.Name = "BtnKaydet";
+            this.BtnKaydet.Size = new System.Drawing.Size(75, 61);
+            this.BtnKaydet.TabIndex = 1;
+            this.BtnKaydet.Text = "Yenile";
+            this.BtnKaydet.UseVisualStyleBackColor = true;
+            this.BtnKaydet.Click += new System.EventHandler(this.BtnKaydet_Click);
+            // 
+            // BtnSil
+            // 
+            this.BtnSil.Location = new System.Drawing.Point(84, 224);
+            this.BtnSil.Name = "BtnSil";
+            this.BtnSil.Size = new System.Drawing.Size(75, 61);
+            this.BtnSil.TabIndex = 1;
+            this.BtnSil.Text = "Sil";
+            this.BtnSil.UseVisualStyleBackColor = true;
+            this.BtnSil.Click += new System.EventHandler(this.BtnSil_Click);
+            // 
+            // BtnYukle
+            // 
+            this.BtnYukle.Location = new System.Drawing.Point(468, 224);
+            this.BtnYukle.Name = "BtnYukle";
+            this.BtnYukle.Size = new System.Drawing.Size(75, 61);
+            this.BtnYukle.TabIndex = 1;
+            this.BtnYukle.Text = "Yükle";
+            this.BtnYukle.UseVisualStyleBackColor = true;
+            this.BtnYukle.Click += new System.EventHandler(this.BtnYukle_Click);
+            // 
+            // BtnIptal
+            // 
+            this.BtnIptal.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+            this.BtnIptal.Location = new System.Drawing.Point(549, 224);
+            this.BtnIptal.Name = "BtnIptal";
+            this.BtnIptal.Size = new System.Drawing.Size(75, 61);
+            this.BtnIptal.TabIndex = 1;
+            this.BtnIptal.Text = "İptal";
+            this.BtnIptal.UseVisualStyleBackColor = true;
+            this.BtnIptal.Click += new System.EventHandler(this.BtnIptal_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(3, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(621, 199);
+            this.listBox1.TabIndex = 2;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(240, 250);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(157, 17);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "Alt Klasörler Dahil Edilsinmi?";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // AnyOtp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.BtnIptal;
+            this.ClientSize = new System.Drawing.Size(629, 297);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.BtnIptal);
+            this.Controls.Add(this.BtnYukle);
+            this.Controls.Add(this.BtnSil);
+            this.Controls.Add(this.BtnKaydet);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "AnyOtp";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Yüklü Karakter ID Burada Görünecektir.";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button BtnKaydet;
+        private System.Windows.Forms.Button BtnSil;
+        private System.Windows.Forms.Button BtnYukle;
+        private System.Windows.Forms.Button BtnIptal;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,150 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Windows.Forms;
+using Microsoft.VisualBasic;
+using System.IO;
+using System.Diagnostics;
+
+namespace DenemeProjesi
+{
+    public partial class AnyOtp : Form
+    {
+        public AnyOtp()
+        {
+            InitializeComponent();
+        }
+
+        String ID;
+        RegistryKey key1;
+        string fileName;        
+        string seciliDosya;
+
+        //private void ExportRegistryKey(string RegistryKeyPath, string ValueName, string Value, string ExportFileName = "ExportedRegValue.reg")
+        //{
+        //    string regTemplate = @"Windows Registry Editor Version 5.00\r\n[{0}]\r\n""{1}""=""{2}""";
+        //    string regFileContent = string.Format(regTemplate, RegistryKeyPath, ValueName, Value);
+        //    File.WriteAllText(ExportFileName, regFileContent);
+        //    return true;
+        //}
+        static void RunRegFile(string s, string dosyaadi)
+        {
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+            startInfo.FileName = "reg.exe";
+            startInfo.Arguments ="IMPORT " + s;
+            Process.Start(startInfo);
+        }
+        private void altdizinarama(String yol)
+        {
+            string[] dosya;
+            dosya = Directory.GetFiles(yol, "*.reg");
+            for (int i = 0; i < 1; i++)
+            {
+                listBox1.Items.AddRange(dosya);
+                if (checkBox1.Checked == true)
+                {
+                    string[] dizin;
+                    dizin = Directory.GetDirectories(yol);
+                    for (int s = 0; s < dizin.Length; s++)
+                    {
+                        altdizinarama(dizin[s]);
+                    }
+                }
+            }
+        }
+        void griddoldur()
+        {
+            
+                listBox1.Items.Clear();
+                string[] dosya;
+                String yol = Application.StartupPath;// *1
+                dosya = Directory.GetFiles(yol, "*.reg");// *2
+                for (int i = 0; i < 1; i++)
+                {
+                    listBox1.Items.AddRange(dosya);
+                    if (checkBox1.Checked == true)// *3
+                    {
+                        string[] dizin;
+                        dizin = Directory.GetDirectories(yol);
+                        for (int s = 0; s < dizin.Length; s++)
+                        {
+                            altdizinarama(dizin[s]);
+                        }
+                    }
+                    if (listBox1.Items.Count == 0)
+                    {
+                        listBox1.Items.Add("ARANILAN DOSYA TÜRÜ BULUNAMADI!");
+                    }
+                }
+           
+        }
+        void veriYukle()
+        {
+
+            if (listBox1.SelectedItems.Count != 0) { 
+                string path = listBox1.Items[listBox1.SelectedIndex].ToString();
+                byte[] fileBytes = System.IO.File.ReadAllBytes(@path);
+                fileName = Path.GetFileName(path);
+                ID = Path.GetFileNameWithoutExtension(path);
+                seciliDosya = path;
+                key1 = Registry.CurrentUser.OpenSubKey("Software\\AnyOTP");
+                if (key1 != null) Registry.CurrentUser.DeleteSubKey("Software\\AnyOTP");
+                RunRegFile(path,fileName);
+                this.Text = ID;
+            } 
+            else
+            {
+                MessageBox.Show("Lütfen bir Dosya seçip sonra yükleme yapınız.");
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            griddoldur();
+            this.MinimumSize = this.Size;
+            this.MaximumSize = this.Size;
+        }
+
+        private void BtnKaydet_Click(object sender, EventArgs e)
+        {
+            griddoldur();
+        }
+
+        private void BtnSil_Click(object sender, EventArgs e)
+        {
+            
+            if (listBox1.SelectedItems.Count != 0) 
+            {
+                DialogResult dialogResult = MessageBox.Show("Silmek istedğinizden eminmisiniz.", "Silme İşlemi", MessageBoxButtons.YesNo);
+                if (dialogResult == DialogResult.Yes) File.Delete(seciliDosya); 
+            }
+            else { MessageBox.Show("Lütfen silmek istediğiniz dosyayı seçiniz."); }
+            griddoldur();
+        }
+
+        private void BtnIptal_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void BtnYukle_Click(object sender, EventArgs e)
+        {
+            veriYukle();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (listBox1.SelectedItems.Count != 0)
+            {
+                string path = listBox1.Items[listBox1.SelectedIndex].ToString();
+                byte[] fileBytes = System.IO.File.ReadAllBytes(@path);
+                fileName = Path.GetFileName(path);
+                ID = Path.GetFileNameWithoutExtension(path);
+                seciliDosya = path;
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
